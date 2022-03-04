c94d0789f830ae805263eec187c592a38d5f46a5
Faizan Siddiqui <siddiquifaizan248@gamil.com>
2022-03-05 00:50:50 +0530

    fixed opcaity

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const minTemp = document.querySelector('.min-temp');
 const maxTemp = document.querySelector('.max-temp');
 const searchQuery = document.querySelector('.search-query');
 const weatherIcon = document.querySelector('.weather-icon');
+const contentBox=document.querySelector('.content');
 this.showCurrrentLocaationWeather();
 
 //Show current Date
@@ -41,6 +42,7 @@ searchQuery.addEventListener('keypress', function (e) {
 function showCurrrentLocaationWeather() {
   navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
+    contentBox.classList.remove("hidden");
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=07910dfd9ca40f69d0255d54b48baeeb`).then(res => {
       weatherDescription.innerHTML = res.data.weather[0].description;
       temp.innerHTML = parseInt(res.data.main.temp - 273);
@@ -50,6 +52,10 @@ function showCurrrentLocaationWeather() {
       weatherIcon.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
       place.innerHTML = res.data.name;
     })
-  });
+  },error=>{
+    if (error.code == error.PERMISSION_DENIED)
+      alert("Please enable your location");
+  }
+  );
 };
 
