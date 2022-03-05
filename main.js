8c8ba93976207d54d44c8eaaf0371c582ceade23
Faizan Siddiqui <siddiquifaizan248@gamil.com>
2022-03-05 13:20:59 +0530

    Added Fixed javascript

--- main.js
+++ main.js
@@ -63,17 +63,17 @@ function showCurrrentLocaationWeather() {
   navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
    
-    // axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=07910dfd9ca40f69d0255d54b48baeeb`).then(res => {
-    //   contentBox.classList.remove("hidden");
-        //  errorBox.classList.add("hidden");
-    //   weatherDescription.innerHTML = res.data.weather[0].description;
-    //   temp.innerHTML = parseInt(res.data.main.temp - 273);
-    //   minTemp.innerHTML = parseInt(res.data.main.temp_min - 273);
-    //   maxTemp.innerHTML = parseInt(res.data.main.temp_max - 273);
-    //   const iconCode = res.data.weather[0].icon;
-    //   weatherIcon.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
-    //   place.innerHTML = res.data.name;
-    // })
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=07910dfd9ca40f69d0255d54b48baeeb`).then(res => {
+      contentBox.classList.remove("hidden");
+         errorBox.classList.add("hidden");
+      weatherDescription.innerHTML = res.data.weather[0].description;
+      temp.innerHTML = parseInt(res.data.main.temp - 273);
+      minTemp.innerHTML = parseInt(res.data.main.temp_min - 273);
+      maxTemp.innerHTML = parseInt(res.data.main.temp_max - 273);
+      const iconCode = res.data.weather[0].icon;
+      weatherIcon.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+      place.innerHTML = res.data.name;
+    })
   },error=>{
     if (error.code == error.PERMISSION_DENIED)
       alert("Please enable your location");
