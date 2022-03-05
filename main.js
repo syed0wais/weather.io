abfeedaca03a583e46a9da7df822b07fd683b0ad
Faizan Siddiqui <siddiquifaizan248@gamil.com>
2022-03-05 13:38:34 +0530

    Almost FInal Push

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ const minTemp = document.querySelector('.min-temp');
 const maxTemp = document.querySelector('.max-temp');
 const searchQuery = document.querySelector('.search-query');
 const weatherIcon = document.querySelector('.weather-icon');
-const contentBox=document.querySelector('.content');
-const errorMeesage=document.querySelector('.error-message');
+const contentBox = document.querySelector('.content');
+const errorMeesage = document.querySelector('.error-message');
 this.showCurrrentLocaationWeather();
-const errorBox=document.querySelector('.error');
+const errorBox = document.querySelector('.error');
 
 //Show current Date
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -22,7 +22,10 @@ console.log(dateString);
 date.innerHTML = dateString;
 
 
-
+//Utility Functions
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 
 
@@ -35,22 +38,25 @@ searchQuery.addEventListener('keypress', function (e) {
     console.log(searchQuery.value);
     // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchQuery.value}&appid=07910dfd9ca40f69d0255d54b48baeeb`).then(res => {
-      errorMeesage.innerHTML=""
-      errorBox.classList.remove("hidden");
-      contentBox.classList.add("hidden");
-      weatherDescription.innerHTML = res.data.weather[0].description;
+      errorMeesage.innerHTML = ""
+      errorBox.classList.add("hidden");
+      contentBox.classList.remove("hidden");
+
+      weatherDescription.innerHTML = capitalizeFirstLetter(res.data.weather[0].description);
       temp.innerHTML = parseInt(res.data.main.temp - 273);
       minTemp.innerHTML = parseInt(res.data.main.temp_min - 273);
       maxTemp.innerHTML = parseInt(res.data.main.temp_max - 273);
       const iconCode = res.data.weather[0].icon;
       weatherIcon.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
       place.innerHTML = res.data.name;
-    }).catch(()=>{
+      searchQuery.value=""
+    }).catch(() => {
+      contentBox.classList.add("hidden");
       errorBox.classList.remove("hidden");
-      errorMeesage.innerHTML=`${searchQuery.value} Not Found 😔`
+      errorMeesage.innerHTML = `${searchQuery.value} Not Found 😔`
 
     })
-    
+
   }
 });
 // console.log(searchQuery);
@@ -62,19 +68,20 @@ searchQuery.addEventListener('keypress', function (e) {
 function showCurrrentLocaationWeather() {
   navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
-   
+
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=07910dfd9ca40f69d0255d54b48baeeb`).then(res => {
-      contentBox.classList.remove("hidden");
-         errorBox.classList.add("hidden");
-      weatherDescription.innerHTML = res.data.weather[0].description;
+
+      weatherDescription.innerHTML = capitalizeFirstLetter(res.data.weather[0].description);
       temp.innerHTML = parseInt(res.data.main.temp - 273);
       minTemp.innerHTML = parseInt(res.data.main.temp_min - 273);
       maxTemp.innerHTML = parseInt(res.data.main.temp_max - 273);
       const iconCode = res.data.weather[0].icon;
       weatherIcon.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
       place.innerHTML = res.data.name;
+      contentBox.classList.remove("hidden");
+      errorBox.classList.add("hidden");
     })
-  },error=>{
+  }, error => {
     if (error.code == error.PERMISSION_DENIED)
       alert("Please enable your location");
   }
